707b0a739ab3ca8d8bcb043f9fa479289e9a5773
A.P <116370713+COCO-DEV03033@users.noreply.github.com>
2026-02-19 14:25:17 -0800

    Update dinoPrices.js

--- config/dinoPrices.js
+++ config/dinoPrices.js
@@ -1,5 +1,23 @@
 // Sell prices for stored dinos (edit later when client gives list)
 module.exports = {
-  Rex: 100,
-  Spino: 120
+  Diabloceratops: 750,
+  Dryosaurus: 150,
+  Hypsilophodon: 150,
+  Maiasaura: 900,
+  Pachycephalosaurus: 400,
+  Stegosaurus: 2000,
+  Tenontosaurus: 750,
+  Triceratops: 2500,
+  Carnotaurus: 700,
+  Ceratosaurus: 700,
+  Deinosuchus: 600,
+  Dilophosaurus: 450,
+  Herrerasaurus: 350,
+  Omniraptor: 600,
+  Pteranodon: 250,
+  Troodon: 100,
+  Beipiaosaurus: 350,
+  Gallimimus: 250,
+  Tyrannosaurus: 2500,
+  Allosaurus: 1500
 };
